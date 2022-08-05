05656b1e5ff74c64b9fb84fce1bc0231d24389e9
thanhnguyen <chithanh12@gmail.com>
2022-08-05 16:40:07 +0700

    Update key layout

--- config/include/keycodes.h
+++ config/include/keycodes.h
@@ -6,7 +6,7 @@
 #define ZLGUI &bht LGUI Z
 #define SLHRGUI &bht RGUI FSLH
 
-#define HYPTAB &bht LS(LC(LGUI)) TAB       //
+#define HYPTAB &bht LS(LC(LA(LGUI))) TAB       //
 
 #define ______ &trans
 
@@ -19,17 +19,13 @@
 #define L1_A    &hl_l            L1    A
 #define R1_ESC  &lthp            R1    ESCAPE
 
-#define RGUI_H  &hm_retro_slow_r RGUI  H
-#define RSFT_J  &hm_fast_r       RSHFT J
-#define RCTL_K  &hm_fast_r       RCTRL K
-#define RALT_L  &hm_r            RALT  L
-#define L1_SEM  &hl_fast_r       L1    SEMI
-#define R1_BSPC &lthp            R1    BSPC
+#define RGUI_DOT  &hm_retro_slow_r RGUI  DOT
+#define RSFT_J    &hm_fast_r       RSHFT J
+#define RCTL_FSLH &hm_fast_r    RCTRL FSLH
+#define RALT_SEMI &hm_r         RALT  SEMI
+#define R1_BSPC   &lthp         R1    BSPC
 
 #define L1_SPC  &ltp L1 SPACE
-
-
-
 
 #define LG__  &hm_retro_slow_l LGUI
 #define LS__  &hm_l            LSHFT
